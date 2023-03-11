b3dc31664bdcc53e842d2b4e697aa325db3f0ce5
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-03-11 20:50:05 +0400

    setup rtk query

--- server/src/routes/generalRoutes.ts
+++ server/src/routes/generalRoutes.ts
@@ -1,5 +1,9 @@
 import { Router } from 'express'
 
+import { getUser } from '../controllers/generalControllers.js'
+
 const router = Router()
+
+router.get('/uesr/:id', getUser)
 
 export default router
