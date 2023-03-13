d7ed05fc4118a8f326e025e09e8410fcd988da6e
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-03-14 03:44:48 +0400

    finish geography page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import Dashboard from './pages/Dashboard'
 import Products from './pages/Products'
 import Customers from './pages/Customers'
 import Transactions from './pages/Transactions'
+import Geography from './pages/Geography'
 
 function App() {
   const { mode } = useSelector((state: AppState) => state.global)
@@ -29,6 +30,7 @@ function App() {
               <Route path="/products" element={<Products />} />
               <Route path="/customers" element={<Customers />} />
               <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
             </Route>
           </Routes>
         </ThemeProvider>
--- client/src/store/api.ts
+++ client/src/store/api.ts
@@ -3,6 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type Product from '../types/Product'
 import type User from '../types/User'
 import type Transactions from '../types/Transaction'
+import type Geography from '../types/Geography'
 
 interface TransactionsParams {
   page: number
@@ -14,7 +15,7 @@ interface TransactionsParams {
 const api = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_BASE_URL }),
   reducerPath: 'adminApi',
-  tagTypes: ['User', 'Products', 'Customers', 'Transactions'],
+  tagTypes: ['User', 'Products', 'Customers', 'Transactions', 'Geography'],
   endpoints: build => ({
     getUser: build.query<User, string>({
       query: id => `/api/general/user/${id}`,
@@ -36,6 +37,10 @@ const api = createApi({
       }),
       providesTags: ['Transactions'],
     }),
+    getGeography: build.query<Geography[], void>({
+      query: () => '/api/client/geography',
+      providesTags: ['Geography'],
+    }),
   }),
 })
 
@@ -44,6 +49,7 @@ export const {
   useGetProductsQuery,
   useGetCustomersQuery,
   useGetTransactionsQuery,
+  useGetGeographyQuery,
 } = api
 
 export default api
