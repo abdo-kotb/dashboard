2fc7a32437e4b67b5245e82bdc06f721aeb97d14
abdo-kotb <86558336+abdo-kotb@users.noreply.github.com>
2023-03-15 00:29:52 +0400

    getPerformance endpoint (backend)

--- server/src/controllers/managementControllers.ts
+++ server/src/controllers/managementControllers.ts
@@ -1,8 +1,47 @@
 import asyncHandler from 'express-async-handler'
+import mongoose from 'mongoose'
+
 import User from '../models/userModel.js'
 
 export const getAdmins = asyncHandler(async (_, res) => {
   const admins = await User.find({ role: 'admin' }).select('-password')
 
   res.status(200).json(admins)
+})
+
+export const getUserPerformance = asyncHandler(async (req, res) => {
+  const { id } = req.params
+
+  const userWithStatsAndTransactions = (
+    await User.aggregate([
+      { $match: { _id: new mongoose.Types.ObjectId(id) } },
+      {
+        $lookup: {
+          from: 'affiliatestats',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'affiliateStats',
+        },
+      },
+      { $unwind: '$affiliateStats' },
+      {
+        $lookup: {
+          from: 'transactions',
+          let: { affiliateSales: '$affiliateStats.affiliateSales' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $in: ['$_id', '$$affiliateSales'],
+                },
+              },
+            },
+          ],
+          as: 'affiliateStats.affiliateSales',
+        },
+      },
+    ])
+  )[0]
+
+  res.status(200).json(userWithStatsAndTransactions)
 })
--- server/src/routes/managementRoutes.ts
+++ server/src/routes/managementRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express'
 
-import { getAdmins } from '../controllers/managementControllers.js'
+import {
+  getAdmins,
+  getUserPerformance,
+} from '../controllers/managementControllers.js'
 
 const router = Router()
 
 router.get('/admins', getAdmins)
+router.get('/performance/:id', getUserPerformance)
 
 export default router
